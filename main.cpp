1a6b2e1465cc78b0772e55bca54622e5bc072c68
Eurry <92621710+PyFider@users.noreply.github.com>
2022-05-08 19:34:27 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,16 @@
+﻿# include <iostream>
+# include <Windows.h>
+#include <cstdlib>
+
+using namespace std;
+
+int main() {
+	while (true) {
+		SYSTEMTIME time;
+		GetLocalTime(&time);
+		cout << "Today| " << time.wDay << "." << time.wMonth << "." << time.wYear;
+		cout << endl << "      " << time.wHour << ":" << time.wMinute << ":" << time.wSecond << endl << endl;
+		Sleep(1000); // wait 1 second		
+		system("cls"); // clear console
+	}
+}
